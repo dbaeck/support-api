9233ba4a60791618de3ee78d74c0f77d3d668d99
Daniel Baeck <db@sharpsrc.net>
2015-06-30 14:59:37 +0200

    starting with migrations

---
+++ support/database/migrations/2015_05_06_131144_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function($table)
+		{
+		    $table->increments('id');
+		    $table->string('name');
+            $table->integer('user_id')->unsigned();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
