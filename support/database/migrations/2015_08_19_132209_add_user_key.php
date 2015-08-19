51dc0e437242ced5308b98248961dcd9429108f3
Daniel Baeck <db@sharpsrc.net>
2015-08-19 15:28:37 +0200

    add api key migration

---
+++ support/database/migrations/2015_08_19_132209_add_user_key.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->string('api_key', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn('api_key');
+        });
+    }
+}
