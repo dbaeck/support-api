ef6ba8a339ff8bb5dc36539d0407ba01cc30daab
Daniel Baeck <db@sharpsrc.net>
2015-08-19 15:22:33 +0200

    add ide_helper and basic models

--- support/app/Http/routes.php
+++ support/app/Http/routes.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+
+use App\State;
 
 Route::get('/', function () {
-    return view('welcome');
+    dump(State::all());
+    //return view('welcome');
+});
+
+Route::group(['as' => 'api::'], function(){
+
 });
